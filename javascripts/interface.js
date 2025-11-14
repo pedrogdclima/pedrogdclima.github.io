4c37f2c3600c89c31c4aec0c0e0f5784d6764ac4
Pedro Lima <pedrogdcl@gmail.com>
2025-11-14 12:31:18 -0500

    Add toggle curriculum functionality

--- javascripts/interface.js
+++ javascripts/interface.js
@@ -1,2 +1,18 @@
 
-alert('hello');
+let curriculum;
+
+function main() {
+  curriculum = document.querySelector('#curriculum');
+  setupEventListeners();
+}
+
+function setupEventListeners() {
+  curriculum.addEventListener('click', handleCurriculumClick);
+}
+
+function handleCurriculumClick(event) {
+  event.preventDefault();
+  this.nextElementSibling.classList.toggle('hide');
+}
+
+document.addEventListener('DOMContentLoaded', main);
